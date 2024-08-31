894832054392832882c3008578cc87b11891e654
J  Meghana Redy <spacemeghana@gmail.com>
2024-08-31 16:19:30 +0530

    Initial commit with project files

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,53 @@
+
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard({onSelectSquare, board}) {
+    
+    // let gameBoard = initialGameBoard;
+
+    // for(const turn of turns) {
+    //     const { square, player } = turn;
+    //     const { row, col } = square;
+        
+    //     gameBoard[row][col] = player;
+    //     }
+    
+
+    // const [gameBoard, setGameBoard] =  useState(initialGameBoard);
+
+    // function handleSelectSquares(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //        return updatedBoard;
+    //     } );
+
+    //     onSelectSquare();
+    // }
+ 
+    return (
+        <ol id="game-board">
+            {
+                board.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                        <li key={colIndex}>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                            disabled={playerSymbol !== null} 
+                            >
+                            {playerSymbol}</button>
+                        </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+
+    );
+}
